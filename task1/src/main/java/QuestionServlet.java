7de7c4aefc480ac51a31a0f2ab4bf0bb307113f5
Bohdan <shubabohdan@gmail.com>
2019-01-27 22:00:47 +0200

    commit 2

--- task1/src/main/java/QuestionServlet.java
+++ task1/src/main/java/QuestionServlet.java
@@ -10,8 +10,6 @@ public class QuestionServlet extends HttpServlet {
             + "<head><title>Prog.kiev.ua</title></head>"
             + "<body><h1>%s</h1></body></html>";
 
-    final
-
-rff
+    final static
 }
 
