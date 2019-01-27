ce84e2e80e4f7664378e61ab24ae61b823629f90
Bohdan <shubabohdan@gmail.com>
2019-01-27 21:24:10 +0200

    Initial commit

---
+++ task1/src/main/java/QuestionServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.http.HttpServlet;
+
+public class QuestionServlet extends HttpServlet {
+    static final int QUESTION_1_YES = 0;
+    static final int QUESTION_1_NO = 1;
+    static final int QUESTION_2_YES = 0;
+    static final int QUESTION_2_NO = 1;
+
+    static final String TEMPLATE = "<html>"
+            + "<head><title>Prog.kiev.ua</title></head>"
+            + "<body><h1>%s</h1></body></html>";
+
+    final
+
+
+}
