fab8bbac2ce840688b6ecd5e5c73cbc6b86067f1
Bohdan <shubabohdan@gmail.com>
2019-03-18 02:29:36 +0200

    reading data with table

--- task4/src/main/java/ua/bohdan/Main.java
+++ task4/src/main/java/ua/bohdan/Main.java
@@ -1,18 +1,37 @@
 package ua.bohdan;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Session session = HibernateUtil.getSession();
-          //  session.beginTransaction();
+            session.beginTransaction();
             try {
 
-                Menu dish = new Menu("Ravioli8", 2.5, 100, true);
-                session.save(dish); // списали грощі з клієнта
-                Menu dish2 = new Menu("Ravioli2", 2.5, 100, true); // зарахували на карту
+                Menu dish = new Menu("Ravioli", 3.3, 250, false);
+                session.save(dish);
+                Menu dish2 = new Menu("Noodles", 1.1, 200, true);
                 session.save(dish2);
+                Menu dish3 = new Menu("Salad", 1.1, 150, false);
+                session.save(dish3);
+                Menu dish4 = new Menu("Soup", 2.5, 220, true);
+                session.save(dish4);
+                Menu dish5 = new Menu("Lasagna", 3.5, 250, false);
+                session.save(dish5);
+                Menu dish6 = new Menu("Ratatouille", 3.2, 150, false);
+                session.save(dish6);
+                Menu dish7 = new Menu("Ramen", 4.1, 450, true);
+                session.save(dish7);
+                Menu dish8 = new Menu("Cheesecake", 1.35, 100, true);
+                session.save(dish8);
+                Menu dish9 = new Menu("Roast beef", 5.25, 300, true);
+                session.save(dish9);
+                Menu dish10 = new Menu("Ice cream", 1.45, 120, false);
+                session.save(dish10);
                 session.getTransaction().commit();
             } catch (Exception e) {
                 session.getTransaction().rollback();
@@ -22,6 +41,27 @@ public class Main {
         } finally {
             HibernateUtil.destroy();
         }
+
+        List<Menu> list = null;
+        try {
+            Session session = HibernateUtil.getSession();
+            session.beginTransaction();
+            try {
+                Query query = session.createQuery("From menu");
+                list = (List<Menu>) query.list();
+            } catch (Throwable e) {
+                session.getTransaction().rollback();
+            }
+            session.close();
+        } finally {
+            HibernateUtil.destroy();
+        }
+        if (list != null && !list.isEmpty()) {
+            for (Menu menu : list) {
+                System.out.println(menu);
+            }
+        }
+
     }
 
 }
